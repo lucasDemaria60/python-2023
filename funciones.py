1c26034157f30017428e8478682f3025ae85fe60
lucasDemaria60 <131877186+lucasDemaria60@users.noreply.github.com>
2023-05-29 12:57:04 -0300

    agrego función para verificar opciones

--- funciones.py
+++ funciones.py
@@ -10,3 +10,18 @@ def checkNum(num):
             except ValueError:
                 print (f"El valor '{num}' no es un numero")
                 num = input("Ingrese un número valido: ")
+
+def checkOption(opcion,cantidad):
+    while True:
+        try:
+            opcion = int(opcion)
+            for op in range(cantidad+1):
+                if op == 0:
+                    continue
+                if opcion==op:
+                    return opcion
+            print("Valor fuera de rango")
+            opcion = input("Ingrese opcion valida: ")
+        except ValueError:
+            print("Valor invalido")
+            opcion = input("Ingrese opcion valida: ")
